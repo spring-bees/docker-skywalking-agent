8b92d732ae503c3bc29bdc64c6e7b5707262b07d
Lei Zhang <zhanglei@apache.org>
2020-09-21 11:14:28 +0800

    init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-if [ $SW_AGENT_COLLECTOR_BACKEND_SERVICES ]; then
-  echo "SW_AGENT_NAME=$SW_AGENT_NAME"
-  echo "SW_AGENT_COLLECTOR_BACKEND_SERVICES=$SW_AGENT_COLLECTOR_BACKEND_SERVICES"
-  export JAVA_OPTS="${JAVA_OPTS} -javaagent:${SKYWALKING_AGENT} -DSW_AGENT_NAME=${SW_AGENT_NAME} -DSW_AGENT_COLLECTOR_BACKEND_SERVICES=${SW_AGENT_COLLECTOR_BACKEND_SERVICES}"
+if [ $SW_AGENT_ENABLED ]; then
+
+  if [ $SW_AGENT_ENABLED = 'true' ]; then
+
+    if [ -z $SW_AGENT_COLLECTOR_BACKEND_SERVICES ]; then
+      echo 'ERROR environment variable SW_AGENT_COLLECTOR_BACKEND_SERVICES must not be empty'
+      exit 1
+    else
+      echo 'SW_AGENT_COLLECTOR_BACKEND_SERVICES=$SW_AGENT_COLLECTOR_BACKEND_SERVICES'
+    fi
+
+    if [ -z $SW_AGENT_NAME ]; then
+      echo 'ERROR environment variable SW_AGENT_NAME must not be empty'
+      exit 1
+    else
+      echo 'SW_AGENT_NAME=$SW_AGENT_NAME'
+    fi
+
+    export JAVA_OPTS="${JAVA_OPTS} -javaagent:${SKYWALKING_AGENT} -DSW_AGENT_NAME=${SW_AGENT_NAME} -DSW_AGENT_COLLECTOR_BACKEND_SERVICES=${SW_AGENT_COLLECTOR_BACKEND_SERVICES}"
+    echo 'JAVA_OPTS=${JAVA_OPTS}'
+
+  fi
+
 fi
 
 exec "$@"
